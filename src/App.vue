ee73af1dcc079b75f0a2a918650b62b755d8d7df
hajjamn <hajjamn@gmail.com>
2024-04-22 20:38:36 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from './store.js'
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <div>
+    {{ store.message }}
+  </div>
+
+</template>
+
+<style lang="scss">
+@use './assets/style/general.scss'
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(fas, far)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
