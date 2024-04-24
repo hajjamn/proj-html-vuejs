8215a9c818c18cd54e6ef997cedc2a99c46de589
hajjamn <hajjamn@gmail.com>
2024-04-24 10:46:40 +0200

    ADD: special moments section

--- src/components/content/special moments/SpecialMoments.vue
+++ src/components/content/special moments/SpecialMoments.vue
@@ -1,9 +1,14 @@
 <script>
+import SpecialMomentsCard from './SpecialMomentsCard.vue'
+import SpecialMomentsData from '../../../data/specialmoments.json'
 
 export default {
+  components: {
+    SpecialMomentsCard
+  },
   data() {
     return {
-
+      SpecialMomentsData
     }
   }
 }
@@ -12,9 +17,26 @@ export default {
 <template>
   <section>
     <div class="my-container my-container-small">
-      <div></div>
+      <div class="text-center mb-5">
+        <p class="before-title">CORPORATE & WEDDINGS</p>
+        <h3>Baking Special Moments</h3>
+      </div>
     </div>
   </section>
+  <div class="my-container my-container-small">
+    <div class="row my-row">
+      <SpecialMomentsCard v-for="data in SpecialMomentsData.cardData" :item="data"></SpecialMomentsCard>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+.my-row {
+  gap: 100px;
+}
+
+h3 {
+  color: var(--fifth_color);
+  font-weight: 700;
+}
+</style>
---
+++ src/components/content/special moments/SpecialMomentsCard.vue
@@ -0,0 +1,45 @@
+<script>
+
+export default {
+  data() {
+    return {
+      hover: false
+    }
+  },
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<template>
+  <div class="col position-relative" @mouseover="hover = true" @mouseleave="hover = false">
+    <img :src="hover === false ? `/img/${item.firstImage}` : `/img/${item.secondImage}`" alt="">
+    <div class="my-overlay" :class="hover === true ? 'my-active' : ''">
+      <p class=" before-title">{{ item.description }}</p>
+      <h2 class="text-white my-4">{{ item.title }}</h2>
+      <button class="my-btn my-btn-purple">Explore more</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.col {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.my-overlay {
+  text-align: center;
+  position: absolute;
+  margin: auto;
+  padding: 0 12px;
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+
+.my-active {
+  opacity: 1;
+}
+</style>
